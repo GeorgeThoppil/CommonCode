f1682d5f994e26bc16592e8af81e7d8b0749f883
killzon567 <georgethoppil2@gmail.com>
2014-12-27 22:00:13 -0500

    Changes

--- server.js
+++ server.js
@@ -1,21 +1,37 @@
-/**
- * 
- */
-
-
+var express = require('express'),jade = require('jade');
 var http = require('http');
+var https = require('https');
 var fs = require('fs');
-var url = require('url');
-var index = fs.readFileSync('index.html');
 
-http.createServer(function (req,res)
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);
+app.set('views', './views');
+app.engine('jade', jade.__express);
+//app.engine('html', require('ejs').renderFile);
+
+app.set('view engine', 'jade');
+
+
+app.get('/Projects',function(req,res)
 		{
-	
-	var urlobj = url.parse(req.url,true,false);
-	
-	
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	  res.end(index);
-	  console.log(urlobj);
-		}).listen(8080);
+		res.send('Hello from Projects Page');
+		});
 
+app.get('/Contact',function(req,res)
+		{
+		res.send('Hello from Contact Page');
+		
+		});
+
+
+app.get('/',function(req,res)
+		{
+		//res.send('Hello from main page');
+		res.render('index');
+		});
+
+http.createServer(app).listen(app.get('port'), function(){
+	  console.log('Express server listening on port ' + app.get('port'));
+	});
